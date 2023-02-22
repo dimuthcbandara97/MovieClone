b7df432d3bd2b91ee3d97a0a32692b2d68b34c62
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-02-22 19:32:52 +0530

    Initial Commit

---
+++ MovieClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieClone
+//
+//  Created by Dimuth Bandara on 2023-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
